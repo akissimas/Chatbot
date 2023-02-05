78553e7c75500d274c594e8d2402acb3686c4825
akis <akissimas16@gmail.com>
2023-02-05 18:20:51 +0200

    update suggestion.py

--- suggestion.py
+++ suggestion.py
@@ -4,14 +4,14 @@ import random
 
 class Suggestion:
     def __init__(self):
-        dataset = pd.read_csv("./datasets/suggestions.csv")
+        dataset = pd.read_csv("../datasets/suggestions.csv")
 
         self.dataset_dict = dict(zip(dataset["Text"], dataset["Emotion"]))
 
 
     def search(self, value):
-        result = [k for k, v in self.dataset_dict.items() if v == value]
-        return random.choice(result)
+        res = [k for k, v in self.dataset_dict.items() if v == value]
+        return random.choice(res)
 
 
 if __name__ == '__main__':
