9a68fe83dec18d61db80f86f22960e1402d71212
akis <akissimas16@gmail.com>
2023-06-15 22:28:32 +0300

    update script.js

--- app/static/scripts/script.js
+++ app/static/scripts/script.js
@@ -3,7 +3,7 @@ const submitBtn = document.getElementById("submitBtn");
 const msgChat = document.querySelector(".msg-chat");
 
 const USER = "You";
-const BOT = "Nerdbot";
+const BOT = "ChatBot";
 const USER_IMG = "https://cdn-icons-png.flaticon.com/512/2922/2922510.png";
 const BOT_IMG = "https://cdn-icons-png.flaticon.com/512/4712/4712106.png";
 
@@ -21,10 +21,28 @@ submitBtn.addEventListener("click", (ev) => {
   const msgText = userInput.value;
   if (!msgText) return;
 
+  // get all the messages of the bot
+  let bot_messages = document.querySelectorAll("body > div > main > div.msg.left-msg > div.msg-bubble > div.msg-text");
+
+  // get the last message of the bot
+  let last_bot_message = bot_messages[bot_messages.length - 1].textContent;
+
+  // get all the messages of the user
+  let user_messages = document.querySelectorAll("body > div > main > div.msg.right-msg > div.msg-bubble > div.msg-text");
+
+  // get the last message of the user
+  let last_user_message = user_messages[user_messages.length - 1];
+
+  if (typeof last_user_message === 'object' && last_user_message !== null)
+    last_user_message = last_user_message.textContent
+
+
   appendMessage(USER, msgText, "right", USER_IMG);
   userInput.value = "";
 
-  botResponse(msgText);
+
+
+  botResponse(msgText, last_bot_message, last_user_message);
 });
 
 function appendMessage(name, data, side, img) {
@@ -45,8 +63,8 @@ function appendMessage(name, data, side, img) {
   msgChat.scrollTop += 500;
 }
 
-function botResponse(text) {
-  $.get("/get", { msg: text }).done((data) => {
+function botResponse(text, last_bot_message, last_user_message) {
+  $.get("/get", { msg: text, botMsg: last_bot_message, userMsg: last_user_message }).done((data) => {
     appendMessage(BOT, data, "left", BOT_IMG);
   });
 }
