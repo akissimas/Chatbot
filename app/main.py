56063a9a98cf8c04fa5b1302046be9c87184b3ba
akis <akissimas16@gmail.com>
2023-06-15 22:34:02 +0300

    update main.py

--- app/main.py
+++ app/main.py
@@ -1,31 +1,25 @@
 import numpy as np
 from app.suggestion import Suggestion
-
+from emotion_detection import Emotion
+from Blenderbot import BlenderBot
 # flask imports
 from flask import Flask, render_template, request
 
-# chatterbot imports
-from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
-from emotion_detection import Emotion
 
-
-#  initialization of emotion_detection
+#  initialization of variables
 emotion = Emotion()
 
 emotion.load_model()
 
+suggest = Suggestion()
 
-#  initialization of chatterbot
-chatbot = ChatBot("Chatbot")
-
-# trainer = ChatterBotCorpusTrainer(chatbot)
-#
-# trainer.train(
-#     "chatterbot.corpus.english"
-# )
+bot = BlenderBot()
 
 inputArray = []
+
+past_user_inputs = []
+
+bot_response = []
 
 app = Flask(__name__)
 
@@ -37,16 +31,23 @@ def home():
 
 @app.route("/get")
 def get_bot_response():
-    userText = request.args.get('msg')
+    user_text = request.args.get('msg')
+    past_bot_text = request.args.get('botMsg')
+    past_user_text = request.args.get('userMsg')
+
+    # if past_user_text is None:
+    #     past_user_text = ' '
 
     # find the emotion
-    currentEmotion = emotion.prediction(userText)
+    current_emotion = emotion.prediction(user_text)
 
-    print(currentEmotion)  # TODO: DEBUG
+    print(f"{user_text} : {current_emotion}")  # TODO: DEBUG
 
-    inputArray.append(currentEmotion)
+    inputArray.append(current_emotion)
 
-    # emotion capture of 3 last inputs
+    # initiate the variable which suggest a song
+    reply = None
+    # emotion capture of 5 last inputs
     if len(inputArray) == 3:
         unique, counts = np.unique(inputArray, return_counts=True)
 
@@ -55,22 +56,37 @@ def get_bot_response():
         print(emotion_dict)  # TODO: DEBUG
 
         # get the dominant emotion
-        dominantEmotion = max(emotion_dict, key=emotion_dict.get)
-        print(f"dominant emotion: {dominantEmotion}")  # TODO: DEBUG
+        dominant_emotion = max(emotion_dict, key=emotion_dict.get)
+        print(f"dominant emotion: {dominant_emotion}")  # TODO: DEBUG
 
-        # find and suggest music based on the dominant emotion TODO: na antistoixisw kapoia emotions sto idio (p.x. sadness,fear)
-        suggest = Suggestion()
+        # find and suggest music based on the dominant emotion
+        result = suggest.search(dominant_emotion)
 
-        result = suggest.search(dominantEmotion)
-
-        reply = f"looks like your emotion is {dominantEmotion}.<br>I can suggest you this song:<br>{result}"
+        reply = f"looks like your emotion is {dominant_emotion}.<br>I can suggest you this song:<br>{result}"
 
         inputArray.clear()
 
-        return reply
+
+    # save past user input and bot response
+    global past_user_inputs
+    global bot_response
+
+    if past_user_text is not None:
+        past_user_inputs.append(past_user_text)
+        bot_response.append(past_bot_text)
 
 
-    return str(chatbot.get_response(userText))
+    paylaod = bot.get_payolaod(past_user_inputs, bot_response, user_text)
+
+    # sent the query
+    query = bot.query(paylaod)
+
+    print(query)  # TODO: DEBUG
+
+    if reply is None:
+        return query['generated_text']
+    else:
+        return f"{query['generated_text']}<br><br>{reply}"
 
 
 if __name__ == '__main__':
